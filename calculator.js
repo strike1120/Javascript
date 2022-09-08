7768c9bdb922cfc2edd3c75b9dadc9d01b81263e
strike1120 <70555626+strike1120@users.noreply.github.com>
2022-09-08 12:29:31 +0900

    Create simple calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const calculator = {
+    add : function (a, b) {
+        return(a + b);
+    },
+    minus : function (a, b) {
+        return(a - b);
+    },
+    square : function (a, b) {
+        return(a ** b);
+    }
+
+
+} ;
+
+console.log(calculator.square(2, 3));
+
+
